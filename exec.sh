f979b793c65ac642cfb5daae8176af4363e1bec5
Bruno Marra <bruno.marra@cinnecta.com>
2021-05-24 12:02:10 -0300

    Testa com catalog

--- exec.sh
+++ exec.sh
@@ -1,1 +1,8 @@
-python main.py spec && python main.py check --config secrets/config.json && python main.py discover --config secrets/config.json && python main.py read --config secrets/config.json --catalog integration_tests/configured_catalog.json
+# First build the container
+docker build . -t airbyte/covid-python:dev
+
+# Then use the following commands to run it
+docker run --rm airbyte/covid-python:dev spec
+docker run --rm -v $(pwd)/secrets:/secrets airbyte/covid-python:dev check --config /secrets/config.json
+docker run --rm -v $(pwd)/secrets:/secrets airbyte/covid-python:dev discover --config /secrets/config.json
+docker run --rm -v $(pwd)/secrets:/secrets -v $(pwd)/sample_files:/sample_files airbyte/covid-python:dev read --config /secrets/config.json --catalog /sample_files/configured_catalog.json
